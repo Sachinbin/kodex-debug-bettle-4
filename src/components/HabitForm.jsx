21bc179c87e5df5de54c6e142de17f68dfa64484
Sachin Bind <sachinbind7977@gmail.com>
2026-04-02 03:06:53 +0530

    show card updates

--- src/components/HabitForm.jsx
+++ src/components/HabitForm.jsx
@@ -4,7 +4,7 @@ import { useHabit } from "../context/HabitContext";
 const HabitForm = () => {
   const { addHabit } = useHabit();
 
-  const { register, handleSubmit, reset, formState: { errors }  } = useForm();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
   const onCommit = (values) => {
     const payload = {
@@ -20,15 +20,15 @@ const HabitForm = () => {
   return (
 
     <form className="flex flex-col gap-4"
-
       onSubmit={handleSubmit(onCommit)}>
+
       <div className="flex flex-col">
         <label htmlFor="name">Habit Name</label>
         <input className="border border-0.6 p-1 rounded border-black focus:border-blue-500 outline-none"
-          {...register("name",{required:"Please Enter the name"})} name="name" placeholder="e.g. Morning exercise" />
-          {errors.name && <p className="text-sm text-red-600">{errors.name.message}</p>}
+          {...register("name", { required: "Please Enter the name" })} name="name" placeholder="e.g. Morning exercise" />
+        {errors.name && <p className="text-sm text-red-600">{errors.name.message}</p>}
       </div>
-      
+
 
 
       <div className="flex gap-1 w-70">
@@ -37,9 +37,10 @@ const HabitForm = () => {
           <input className="border border-0.6 p-1 rounded border-black focus:border-blue-500 outline-none w-30"
             {...register("goalValue")} type="number" placeholder="30" />
         </div>
-          <div>
-          <label htmlFor="">Category</label>
-          <select className="border p-1 w-30 rounded border-black focus:border-blue-500 outline-none" >
+
+        <div>
+          <label htmlFor="">Unit</label>
+          <select {...register("unit")} className="border p-1 w-30 rounded border-black focus:border-blue-500 outline-none" >
             <option value="minutes">Minutes</option>
             <option value="pages">Pages</option>
             <option value="reps">Reps</option>
@@ -51,36 +52,36 @@ const HabitForm = () => {
         <div>
           <label htmlFor="">Start Date</label>
           <input className="border border-0.6 p-1 rounded border-black focus:border-blue-500 outline-none w-30"
-            type="date" {...register("dateValue")} />
+            type="date" {...register("date")} />
         </div>
-         <div>
+        <div>
           <label htmlFor="">Category</label>
-          <select {...register("catValues")} className="border p-1 w-30 rounded border-black focus:border-blue-500 outline-none" >
-            <option value="minutes">Health</option>
-            <option value="pages">Focus</option>
-            <option value="reps">Growth</option>
-            <option value="liters">Mindset</option>
+          <select {...register("category")} className="border p-1 w-30 rounded border-black focus:border-blue-500 outline-none" >
+            <option value="Health">Health</option>
+            <option value="Focus">Focus</option>
+            <option value="Growth">Growth</option>
+            <option value="Mindset">Mindset</option>
           </select>
         </div>
       </div>
       <div className="flex flex-col">
         <label htmlFor="">Motivation</label>
-        <textarea {...register("motivValue")} className="border border-0.6 p-1 rounded border-black focus:border-blue-500 outline-none "
-          name="motivationValue" placeholder="Why is this important to you?"></textarea>
+        <textarea {...register("motivation")} className="border border-0.6 p-1 rounded border-black focus:border-blue-500 outline-none "
+           placeholder="Why is this important to you?"></textarea>
       </div>
       <div className="flex flex-col">
         <p>Priority Level</p>
         <div className="flex mt-0 gap-6">
           <div className="flex gap-2">
-            <input {...register("priorityLavel")} type="radio" placeholder="Low" />
+            <input {...register("priorityLavel")} value={"Low"} type="radio" placeholder="Low" />
             <label htmlFor="">Low</label>
           </div>
           <div className="flex gap-2">
-            <input {...register("priorityLavel")} type="radio" placeholder="Low" />
+            <input {...register("priorityLavel")} value={"Medium"} type="radio" placeholder="Low" />
             <label htmlFor="">Medium</label>
           </div>
           <div className="flex gap-2">
-            <input {...register("priorityLavel")} type="radio" placeholder="Low" />
+            <input {...register("priorityLavel")} value={"High"} type="radio" placeholder="Low" />
             <label htmlFor="">High</label>
           </div>
         </div>
--- src/components/HabitItem.jsx
+++ src/components/HabitItem.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useHabit } from "../context/HabitContext";
 
 const HabitItem = ({ habit }) => {
+  console.log(habit)
   const { toggleHabit, deleteHabit, updateHabit, getStreak } = useHabit();
 
   const [editing, setEditing] = useState(false);
@@ -15,6 +16,7 @@ const HabitItem = ({ habit }) => {
     setEditing(false);
   };
 
+
   return (
     <div>
       {editing ? (
@@ -25,17 +27,36 @@ const HabitItem = ({ habit }) => {
           }
         />
       ) : (
-        <div>
-          <h3>{habit.name}</h3>
-          <span>{getStreak(habit.completedDates)}</span>
+        <div className="h-[35%]  shadow-2xl rounded  bg-white p-4">
+          <div className="flex justify-between">
+            <div className="flex gap-5">
+              <p className="text-sm text-blue-500">{habit.category}</p>
+              <p className="text-sm text-amber-600">{habit.priorityLavel}</p>
+            </div>
+            <div className="flex flex-col ">
+              <p className="text-sm">0 <span className="text-amber-600">^^</span></p>
+              <p className="text-[10px] opacity-45">STREAK</p>
+            </div>
+          </div>
+          <h3 className="font-bold text-[18px]">{habit.name}</h3>
+          <h3 className="mt-1">{habit.motivation}</h3>
+          <div className="flex mt-10 justify-between">
+            <div>
+              <p className="text-[12px] opacity-40">GOAL</p>
+              <p className="text-[12px]">{habit.goalValue} min</p>
+            </div>
+           <div className="flex gap-4">
+              <button>Edit</button>
 
-          <button onClick={() => deleteHabit(habit.id)}>
-            Delete
-          </button>
+            <button onClick={() => deleteHabit(habit.id)}>
+              Delete
+            </button>
 
-          <button onClick={() => toggleHabit(habit.id)}>
-            Toggle
-          </button>
+            <button className="bg-blue-700 px-2 text-white font-bold rounded" onClick={() =>  toggleHabit(habit.id)}>
+              Complete
+            </button>
+           </div>
+          </div>
         </div>
       )}
     </div>
